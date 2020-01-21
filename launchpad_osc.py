86cf768d4c9eee56f001d4563f04e9e0eb83ed01
Eldad Roobol <eldad.roobol@chyronhego.com>
2020-01-21 17:08:36 +0100

    start of event handling

--- launchpad_osc.py
+++ launchpad_osc.py
@@ -1,14 +1,34 @@
 import ardour
 import time
-import midi_device
+import midi_device as midi
 
-strip_list = {}
+strips = {}
+
+def mute_strip( id, value ):
+	print( f"mute {id}" )
+
+events = {
+	"mute": mute_strip
+}
 
 def handle_strip_event( id, verb, value ):
 	print( f"strip {id} {verb}: {value}" )
 
+	if verb in events:
+		events[ verb ]( id, value ) 
+	# midi.set_led
+
+def clear_row( row ):
+	print( f"clearing row {row}" )
+
 def strip_list_updated( updated_strips ):
-	global strip_list
+	global strips
+
+	if len( strips ) > len( updated_strips ):
+		# clear excess rows
+		for row in range( len( updated_strips ), len( strips ) ):
+			clear_row( row )
+
 	strips = updated_strips
 	print( "strips updated" )
 	
--- midi_device.py
+++ midi_device.py
@@ -13,21 +13,34 @@ class MidiInputHandler(object):
 		self._wallclock += deltatime
 		print("[%s] @%0.6f %r" % (self.port, self._wallclock, message))
 
+def open_launchpad( device ):
+	ports = device.get_ports()
+
+	for idx,key in enumerate( ports ):
+		print( key )
+
+	matching_port = [ idx for idx,key in enumerate( ports ) if key.startswith( 'Launchpad' ) ]
+
+	if len( matching_port ) == 0:
+		raise Exception( 'no Launchpad input device found' )
+
+	device.open_port( matching_port[0] )
+
 input = rtmidi.MidiIn()
-ports = input.get_ports()
+output = rtmidi.MidiOut()
 
-for idx,key in enumerate( ports ):
-	print( key )
+open_launchpad( input )
+open_launchpad( output )
 
-matching_port = [ idx for idx,key in enumerate( ports ) if key.startswith( 'Launchpad' ) ]
-
-if len( matching_port ) == 0:
-	raise Exception( 'no Launchpad device found' )
-
-input.open_port( matching_port[0] )
 input.set_callback( MidiInputHandler( 'launchpad' ) )
 
 def terminate():
 	global input
 	input.close_port()
 	del input
+
+def set_led( column, row, value ):
+	global output
+	output.send_message( [ 0x90, 0x10 * row + column, value ] )
+
+set_led( 0, 0, 0x0D )
